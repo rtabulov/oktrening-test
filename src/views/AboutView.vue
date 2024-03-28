243bacd12d9bba21a3642fd7868f9cd20828e980
Rassul <rasul.tabulov@gmail.com>
2024-03-28 15:05:22 +0500

    add about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,25 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
+import BaseLink from '@/components/BaseLink.vue';
 
 export default defineComponent({
   name: 'HomeView',
-  components: {},
+  components: {
+    BaseLink,
+  },
 });
 </script>
 
 <template>
-  <div class="container">
-    <h1 class="text-3xl font-bold underline">This is an about page</h1>
+  <div class="container pt-8">
+    <h1 class="text-3xl font-bold">About this project</h1>
+    <p class="mt-8">
+      Built with&nbsp;
+      <BaseLink href="https://vitejs.dev/">Vite</BaseLink>&nbsp;
+      <BaseLink href="https://v2.vuejs.org/">Vue 2.7</BaseLink>&nbsp;
+      <BaseLink href="https://v3.router.vuejs.org/">Vue router</BaseLink>&nbsp;
+      <BaseLink href="https://pinia.vuejs.org/">Pinia</BaseLink>&nbsp;
+      <BaseLink href="https://tailwindcss.com/">Tailwind</BaseLink>
+    </p>
   </div>
 </template>
